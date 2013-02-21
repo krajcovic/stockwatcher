2846703c007fadba6efbdc7daa4c766909fbd200
Krajcovic <dusan.krajcovic@gmail.com>
2013-02-21 15:32:28 +0100

    Stock price

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -1,6 +1,7 @@
 package com.google.gwt.sample.stockwatcher.client;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -8,19 +9,23 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.DateTimeFormat.PredefinedFormat;
+import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.user.client.Random;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 public class StockWatcher implements EntryPoint {
 
+	private static final int REFRESH_INTERVAL = 5000;
 	private VerticalPanel mainPanel = new VerticalPanel();
 	private FlexTable stocksFlexTable = new FlexTable();
 	private HorizontalPanel addPanel = new HorizontalPanel();
@@ -58,6 +63,15 @@ public class StockWatcher implements EntryPoint {
 
 		// Move cursor focus to the input box.
 		newSymbolTextBox.setFocus(true);
+
+		// Setup timer to refresh list automatically.
+		Timer refreshTimer = new Timer() {
+			@Override
+			public void run() {
+				refreshWatchList();
+			}
+		};
+		refreshTimer.scheduleRepeating(REFRESH_INTERVAL);
 
 		// Listen for mouse button
 		addStockButton.addClickHandler(new ClickHandler() {
@@ -115,7 +129,61 @@ public class StockWatcher implements EntryPoint {
 		});
 		stocksFlexTable.setWidget(row, 3, removeStockButton);
 
-		// TODO Get the stock price.
+		// Get the stock price.
+		refreshWatchList();
+
+	}
+
+	private void refreshWatchList() {
+		final double MAX_PRICE = 100.0; // $100.00
+		final double MAX_PRICE_CHANGE = 0.02; // +/- 2%
+
+		StockPrice[] prices = new StockPrice[stocks.size()];
+		for (int i = 0; i < stocks.size(); i++) {
+			double price = Random.nextDouble() * MAX_PRICE;
+			double change = price * MAX_PRICE_CHANGE
+					* (Random.nextDouble() * 2.0 - 1.0);
+
+			prices[i] = new StockPrice(stocks.get(i), price, change);
+		}
+
+		updateTable(prices);
+
+	}
+
+	private void updateTable(StockPrice[] prices) {
+		for (int i = 0; i < prices.length; i++) {
+			updateTable(prices[i]);
+		}
+	}
+
+	private void updateTable(StockPrice stockPrice) {
+		// Make sure the stock is still in the stock table.
+		if (!stocks.contains(stockPrice.getSymbol())) {
+			return;
+		}
+
+		int row = stocks.indexOf(stockPrice.getSymbol()) + 1;
+
+		// Format the data in the Price and Change fields.
+		String priceText = NumberFormat.getFormat("#,##0.00").format(
+				stockPrice.getPrice());
+		NumberFormat changeFormat = NumberFormat
+				.getFormat("+#,##0.00;-#,##0.00");
+		String changeText = changeFormat.format(stockPrice.getChange());
+		String changePercentText = changeFormat.format(stockPrice
+				.getChangePercent());
+
+		// Populate the Price and Change fields with new data.
+		stocksFlexTable.setText(row, 1, priceText);
+		stocksFlexTable.setText(row, 2, changeText + " (" + changePercentText
+				+ "%)");
+
+		// Display timestamp showing last refresh.
+		lastUpdatedLabel.setText("Last update : "
+		// + DateTimeFormat.getMediumDateTimeFormat().format(new Date()));
+				+ DateTimeFormat.getFormat(PredefinedFormat.DATE_TIME_MEDIUM)
+						.format(new Date()));
 
 	}
 }
