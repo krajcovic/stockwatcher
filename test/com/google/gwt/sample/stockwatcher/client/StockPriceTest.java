ff54604d61b1f532aeea372055a74d8f33811cab
Krajcovic <dusan.krajcovic@gmail.com>
2013-02-22 15:35:56 +0100

    Added test skelets for stockprice and stockwathcer for adding test package directory to git.

---
+++ test/com/google/gwt/sample/stockwatcher/client/StockPriceTest.java
@@ -0,0 +1,19 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StockPriceTest {
+
+	@Test
+	public void testStockPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStockPriceStringDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/com/google/gwt/sample/stockwatcher/client/StockWatcherTest.java
@@ -0,0 +1,14 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StockWatcherTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
